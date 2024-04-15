eb389292ab1a5ea94cef6d612b1d7b65adb4a282
juliusyam <juliusworldwide@gmail.com>
2024-04-15 12:23:28 +0100

    Add flight form validation implementation

--- app/Livewire/CreateFlight.php
+++ app/Livewire/CreateFlight.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Http\Requests\FlightRequest;
 use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
@@ -10,23 +11,35 @@ class CreateFlight extends Component
     #[Reactive]
     public array $flights;
 
-    public array $emptyPayload = [
-        'departure_date' => '',
-        'flight_number' => '',
-        'departure_airport' => '',
-        'arrival_airport' => '',
-        'distance' => 0,
-        'airline' => '',
-    ];
+    public string $departure_date = '';
+    public string $flight_number = '';
+    public string $departure_airport = '';
+    public string $arrival_airport = '';
+    public int $distance = 0;
+    public string $airline = '';
 
-    public array $payload;
-
-    public function addFlight() {
-        $this->dispatch('add-flight', payload: $this->payload);
+    protected function rules(): array {
+        return (new FlightRequest())->rules();
     }
 
-    public function __construct() {
-        $this->payload = $this->emptyPayload;
+    public function addFlight() {
+        $this->validate();
+
+        $this->dispatch('add-flight', payload: [
+            'departure_date' => $this->departure_date,
+            'flight_number' => $this->flight_number,
+            'departure_airport' => $this->departure_airport,
+            'arrival_airport' => $this->arrival_airport,
+            'distance' =>$this->distance,
+            'airline' => $this->airline,
+        ]);
+
+        $this->departure_date = '';
+        $this->flight_number = '';
+        $this->departure_airport = '';
+        $this->arrival_airport = '';
+        $this->distance = 0;
+        $this->airline = '';
     }
 
     public function render()
--- resources/views/livewire/create-flight.blade.php
+++ resources/views/livewire/create-flight.blade.php
@@ -3,30 +3,32 @@
         <h2>Add New Flight</h2>
         <div>
             <span>Departure Date:</span>
-            <input type="date" wire:model.live="payload.departure_date">
+            <input type="date" wire:model.live="departure_date">
         </div>
         <div>
             <span>Flight Number:</span>
-            <input type="text" wire:model.live="payload.flight_number">
+            <input type="text" wire:model.live="flight_number">
         </div>
         <div>
             <span>Departure Airport (IATA):</span>
-            <input type="text" wire:model.live="payload.departure_airport">
+            <input type="text" wire:model.live="departure_airport">
         </div>
         <div>
             <span>Arrival Airport (IATA):</span>
-            <input type="text" wire:model.live="payload.arrival_airport">
+            <input type="text" wire:model.live="arrival_airport">
         </div>
         <div>
             <span>Distance (miles):</span>
-            <input type="number" min="0" wire:model.live="payload.distance">
+            <input type="number" min="0" wire:model.live="distance">
         </div>
         <div>
             <span>Airline (ICAO):</span>
-            <input type="text" wire:model.live="payload.airline">
+            <input type="text" wire:model.live="airline">
         </div>
         <button type="button" wire:click="addFlight">Add Flight</button>
     </section>
+
+    <br>
 
     <section>
         @foreach ($flights as $flight)
